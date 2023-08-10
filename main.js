2582bb093cb4b4f97871475074d9571f51741995
Jackob1226 <121788823+Jackob1226@users.noreply.github.com>
2023-08-10 10:43:29 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+document.getElementById('open-modal-btn').addEventListener("click",function() {
+    document.getElementById('my-modal1').classList.add('opened1')
+})
+document.getElementById('cls-btn').addEventListener("click",function (){
+    document.getElementById('my-modal1').classList.remove('opened1')
+})
+document.getElementById('button2').addEventListener('click',function() {
+    document.getElementById('my-modal2').classList.add('open2')
+})
+document.getElementById('cls-btn2').addEventListener('click',function() {
+    document.getElementById('my-modal2').classList.remove('open2')
+})
+document.getElementById('button3').addEventListener('click',function() {
+    document.getElementById('my-modal3').classList.add('open3')
+})
+document.getElementById('cls-btn3').addEventListener('click',function() {
+    document.getElementById('my-modal3').classList.remove('open3')
+})
+document.getElementById('button4').addEventListener('click',function() {
+    document.getElementById('my-modal4').classList.add('open4')
+})
+document.getElementById('cls-btn4').addEventListener('click',function() {
+    document.getElementById('my-modal4').classList.remove('open4')
+})
+document.getElementById('button5').addEventListener('click',function() {
+    document.getElementById('my-modal5').classList.add('open5')
+})
+document.getElementById('cls-btn5').addEventListener('click',function() {
+    document.getElementById('my-modal5').classList.remove('open5')
+})
+document.getElementById('button6').addEventListener('click',function() {
+    document.getElementById('my-modal6').classList.add('open6')
+})
+document.getElementById('cls-btn6').addEventListener('click',function() {
+    document.getElementById('my-modal6').classList.remove('open6')
+})
